2ebd60da579d5c7e517e8ad642c9aada2610f89a
Omulosi <mulongojohnpaul@gmail.com>
2018-08-16 22:30:20 +0300

    Add a post submission field

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, url_for
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from flask import request
 from werkzeug.urls import url_parse
 from datetime import datetime
@@ -14,13 +14,20 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    #posts = Post.query.filter_by(user_id==current_user.id).all()
-    posts = current_user.posts.all()
-    return render_template('index.html', title='Home Page', posts=posts)
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live')
+        return redirect(url_for('index'))
+    posts = current_user.followed_posts().all()
+    return render_template('index.html', title='Home Page',
+            form=form, posts=posts)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
