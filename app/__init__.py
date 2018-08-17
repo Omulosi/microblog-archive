1815595968a847da782c44b564b6823704b897b9
Omulosi <mulongojohnpaul@gmail.com>
2018-08-17 13:41:09 +0300

    Add better date and time support using moment.js lib

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,9 +21,16 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 from app import routes, models, errors
 
+@babel.localselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+# Logging
 if not app.debug:
     # sending logs to emails
     if app.config['MAIL_SERVER']:
